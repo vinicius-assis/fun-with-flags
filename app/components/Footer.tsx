6d9622cab83102adb183abe2fc39f62d2bdd6145
Vinicius Assis <viniciusassis.dev@hotmail.com>
2025-05-12 07:33:57 -0300

    add footer and header components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="py-6 mb-8">
+      <p className="text-center">Made by Vinicius Assis</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Footer, Header } from "./components";
+
 export default function Home() {
   return (
     <>
-      <header>This is the header</header>
+      <Header />
       <main className="flex-1">this is the main</main>
-      <footer>this is the footer</footer>
+      <Footer />
     </>
   );
 }
